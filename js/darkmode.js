2d0f7909258d416046cba1abdda617e7c5c58039
Codemzy <codemzy@gmail.com>
2021-10-11 09:10:07 +0000

    Updated dark mode to only run when switching so removed switch variable

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,37 +1,28 @@
 // sets dark mode
 function setDarkMode(dark, preference) {
-    let switched = false; // know if theme has changed
-    console.log(dark, preference);
     if (dark) { // set dark mode
         // set as the theme if user overriding OS preference (remove theme setting if respecting OS preference)
-        console.log(preference);
         preference !== "dark" ? localStorage.setItem('theme', 'dark') : localStorage.removeItem('theme');
         // change to dark mode by adding class to <html> element
         document.documentElement.classList.add('dark');
-        // set switched so can update button svg
-        switched = true;
     } else if (!dark) { // remove dark mode
         // set as the theme if user overriding OS preference (remove theme setting if respecting OS preference)
         preference !== "light" ? localStorage.setItem('theme', 'light') : localStorage.removeItem('theme');
         // change to light mode by removing class from <html> element
         document.documentElement.classList.remove('dark');
-        // set switched so can update button svg
-        switched = true;
     }
-    // if theme changed update the button by swapping hidden classes on svgs
-    if (switched) {
-        let currentIcon = document.querySelector('#button-dark-mode svg:not(.hidden)');
-        let hiddenIcon = document.querySelector('#button-dark-mode svg.hidden');
-        currentIcon.classList.add('hidden');
-        hiddenIcon.classList.remove('hidden');
-    }
+    // update the button by swapping hidden classes on svgs
+    let currentIcon = document.querySelector('#button-dark-mode svg:not(.hidden)');
+    let hiddenIcon = document.querySelector('#button-dark-mode svg.hidden');
+    currentIcon.classList.add('hidden');
+    hiddenIcon.classList.remove('hidden');
 };
 
 
 // on load
 window.onload = function () {
     const preference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    if (localStorage.getItem('theme') === "dark" || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (localStorage.getItem('theme') === "dark" || (!('theme' in localStorage) && preference === 'dark')) {
         setDarkMode(true, preference);
     }
     document.getElementById('button-dark-mode').addEventListener('click', function() {
