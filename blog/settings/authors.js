53baa492b0b0f32f23285410524005ace29a5262
Codemzy <codemzy@gmail.com>
2021-10-12 13:19:14 +0000

    Updated authors settings comments

--- blog/settings/authors.js
+++ blog/settings/authors.js
@@ -2,9 +2,11 @@
 // const authors = {
 //     "author-id": {
 //         name: "Author Name",
-//         description: "Some information about the author"
+//         description: "Some information about the author", **you can use markup!**", 
+//         metaDescription: "This is optional, but if you used markup in your description, include a plain text description here for SEO",
 //     },
 // }
+// if the blog doesn't need authors, empty the authors object e.g const authors = {};
 
 const authors = {
     "codemzy": {
